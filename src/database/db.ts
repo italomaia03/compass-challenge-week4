6ced0da2a32b59fc146678cfb8332db336f8e9c6
italomaia03 <italo.fernandes.om@gmail.com>
2023-05-31 16:49:55 -0300

    create arrays to save entities in memory

---
+++ src/database/db.ts
@@ -0,0 +1,7 @@
+import { IPet } from "../models/interfaces/IPet";
+import { ITutor } from "../models/interfaces/ITutor";
+
+let tutors: ITutor[] = [];
+let pets: IPet[] = [];
+
+export { tutors, pets };
