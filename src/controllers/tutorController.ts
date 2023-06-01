f7cfa8144b5cc7039c9516d6354171b6510e837c
italomaia03 <italo.fernandes.om@gmail.com>
2023-06-01 14:42:42 -0300

    implement DELETE route for tutors

--- src/controllers/tutorController.ts
+++ src/controllers/tutorController.ts
@@ -20,4 +20,16 @@ function createTutor(req: Request, res: Response) {
     res.status(200).json({ msg: "Tutor successfully created" });
 }
 
-export { getAllTutors, createTutor };
+function deleteTutor(req: Request, res: Response) {
+    const desiredID = Number(req.params.id);
+    const desiredTutor = tutors.find((entity) => {
+        return entity.id === desiredID;
+    });
+    if (!desiredTutor) {
+        return res.status(500).json({ msg: "Fail" });
+    }
+    tutors.splice(tutors.indexOf(desiredTutor), 1);
+    res.status(200).json({ msg: "Tutor deleted" });
+}
+
+export { getAllTutors, createTutor, deleteTutor };
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
-import { getAllTutors, createTutor } from "../controllers/tutorController";
+import {
+    getAllTutors,
+    createTutor,
+    deleteTutor,
+} from "../controllers/tutorController";
 
 export const router: Router = Router();
 
 router.route("/tutors").get(getAllTutors);
 router.route("/tutor").post(createTutor);
+router.route("/tutor/:id").delete(deleteTutor);
