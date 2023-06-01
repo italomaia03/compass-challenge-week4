1f13810cd1f280071620834b3228b45e68d82653
italomaia03 <italo.fernandes.om@gmail.com>
2023-06-01 14:42:07 -0300

    include dist folder in gitignore

--- dist/database/db.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.pets = exports.tutors = void 0;
-let tutors = [];
-exports.tutors = tutors;
-let pets = [];
-exports.pets = pets;
-//# sourceMappingURL=db.js.map
--- dist/routes/router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express_1 = require("express");
-const tutorController_1 = require("../controllers/tutorController");
-exports.router = (0, express_1.Router)();
-exports.router.route("/tutors").get(tutorController_1.getAllTutors);
-exports.router.route("/tutor").post(tutorController_1.createTutor);
-//# sourceMappingURL=router.js.map
