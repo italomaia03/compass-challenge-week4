3dca550212b66eebb24019ae752420b917403be6
italomaia03 <italo.fernandes.om@gmail.com>
2023-06-02 14:18:02 -0300

    add validator to POST /tutor route

--- src/controllers/tutorController.ts
+++ src/controllers/tutorController.ts
@@ -2,23 +2,29 @@ import { Request, Response } from "express";
 import { tutors } from "../database/db";
 import { Tutor } from "../models/models";
 import { ITutor } from "../models/interfaces/ITutor";
+import { validateTutorSchema } from "../utils/tutorValidator";
 
 function getAllTutors(_req: Request, res: Response) {
     res.status(200).json(tutors);
 }
 
-function createTutor(req: Request, res: Response) {
-    const { id, name, phone, email, date_of_birth, zip_code } = req.body;
-    const createdTutor: ITutor = new Tutor(
-        id,
-        name,
-        phone,
-        email,
-        date_of_birth,
-        zip_code
-    );
-    tutors.push(createdTutor);
-    res.status(201).json({ msg: "Tutor has been successfully created" });
+async function createTutor(req: Request, res: Response) {
+    try {
+        const { id, name, phone, email, date_of_birth, zip_code } = req.body;
+        const createdTutor: ITutor = new Tutor(
+            id,
+            name,
+            phone,
+            email,
+            date_of_birth,
+            zip_code
+        );
+        await validateTutorSchema(createdTutor);
+        tutors.push(createdTutor);
+        res.status(201).json({ msg: "Tutor has been successfully created" });
+    } catch (error) {
+        res.status(500).json({ msg: `${error}` });
+    }
 }
 
 function deleteTutor(req: Request, res: Response) {
