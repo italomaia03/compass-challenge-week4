d351cb80872a8df5b2c7915b6d16dc6f4857c923
italomaia03 <italo.fernandes.om@gmail.com>
2023-06-03 13:45:37 -0300

    implement PUT /tutor/:id route

--- src/controllers/tutorController.ts
+++ src/controllers/tutorController.ts
@@ -2,16 +2,19 @@ import { Request, Response } from "express";
 import { tutors } from "../database/db";
 import { Tutor } from "../models/models";
 import { ITutor } from "../models/interfaces/ITutor";
-import { tutorSchema, validateTutorSchema } from "../utils/tutorValidator";
-import { error } from "console";
+import { validateTutorSchema } from "../utils/tutorValidator";
 
 function getAllTutors(_req: Request, res: Response) {
+    tutors.sort((a: ITutor, b: ITutor) => {
+        return a.id - b.id;
+    });
     res.status(200).json(tutors);
 }
 
 async function createTutor(req: Request, res: Response) {
     try {
-        const { id, name, phone, email, date_of_birth, zip_code } = req.body;
+        const { id, name, phone, email, date_of_birth, zip_code } =
+            req.body as ITutor;
         const createdTutor: ITutor = new Tutor(
             Number(id),
             name,
@@ -30,7 +33,45 @@ async function createTutor(req: Request, res: Response) {
         tutors.push(createdTutor);
         res.status(201).json({ msg: "Tutor has been successfully created" });
     } catch (error) {
-        res.status(400).json({ msg: `${error}` });
+        res.status(400).json({ msg: `${(error as Error).message}` });
+    }
+}
+
+async function updateTutor(req: Request, res: Response) {
+    try {
+        const desiredID: number = Number(req.params.id);
+        const { id, name, phone, email, date_of_birth, zip_code } =
+            req.body as ITutor;
+        const desiredTutor = tutors.find((entity) => {
+            return entity.id === desiredID;
+        });
+        if (!desiredTutor) {
+            return res.status(404).json({
+                msg: `Tutor with ID (${desiredID}) has not been found.`,
+            });
+        }
+        const desiredTutorIndex = tutors.indexOf(desiredTutor);
+        const priorTutorPets = desiredTutor.pets;
+        const updatedTutor = new Tutor(
+            id,
+            name,
+            phone,
+            email,
+            date_of_birth,
+            zip_code,
+            priorTutorPets
+        );
+        await validateTutorSchema(updatedTutor);
+        const validateId: boolean = desiredID === updatedTutor.id;
+        if (!validateId) {
+            throw new Error(
+                `Specified ID (${updatedTutor.id}) must not be different from the one passed in the URL parameters.`
+            );
+        }
+        tutors[desiredTutorIndex] = updatedTutor;
+        res.status(200).json({ msg: "Tutor has been successfully updated." });
+    } catch (error) {
+        res.status(400).json({ msg: `${(error as Error).message}` });
     }
 }
 
@@ -48,4 +89,4 @@ function deleteTutor(req: Request, res: Response) {
     res.status(200).json({ msg: "Tutor has been deleted" });
 }
 
-export { getAllTutors, createTutor, deleteTutor };
+export { getAllTutors, createTutor, deleteTutor, updateTutor };
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -3,6 +3,7 @@ import {
     getAllTutors,
     createTutor,
     deleteTutor,
+    updateTutor,
 } from "../controllers/tutorController";
 import { createPet, deletePet, updatePet } from "../controllers/petController";
 
@@ -10,6 +11,6 @@ export const router: Router = Router();
 
 router.route("/tutors").get(getAllTutors);
 router.route("/tutor").post(createTutor);
-router.route("/tutor/:id").delete(deleteTutor);
+router.route("/tutor/:id").put(updateTutor).delete(deleteTutor);
 router.route("/pet/:tutorId").post(createPet);
 router.route("/pet/:petId/tutor/:tutorId").put(updatePet).delete(deletePet);
